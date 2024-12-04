528563b31bbc61afd43f92629029658e8c686722
RealJe-emy <126858904+RealJe-emy@users.noreply.github.com>
2024-12-04 22:55:13 +0300

    Updates

---
+++ scrapper.py
@@ -0,0 +1,14 @@
+import requests 
+import re
+query = input("==>  ")
+data = requests.get(f"https://m.soundcloud.com/search?q={query}")
+
+result = []
+urls = re.findall(r'data-testid="cell-entity-link" href="([^"]+)', data.text)
+photos = re.findall(r'src="(https://i1.sndcdn.com/[^"]+)" data-testid="actual-image"', data.text)
+names = re.findall(r'<div class="Information_CellTitle__2KitR">([^<]+)', data.text)
+for i in range(len(urls)): result.append({'photo': photos[i], 'title': names[i], 'url': f'https://soundcloud.com{urls[i]}'})
+
+for a in result:
+ print(a)
+ print("\n\n")
